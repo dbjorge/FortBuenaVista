e023172e944564146bfe8c0125645010bfedbd7b
Dan Bjorge <dbjorge@gmail.com>
2013-12-15 06:35:06 -0800

    Initial project population/basic folder layout
    Includes initial attempt at a mono-based travis C# config taken from
    http://stackoverflow.com/a/16751773/2825668

---
+++ FortBuenaVista.DesktopApp.Test/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace FortBuenaVista.DesktopApp.Test
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TravisCi_TrivialTest_Passes()
+        {
+            Assert.IsTrue(1 + 1 == 2);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Build Status](https://travis-ci.org/brosmike/FortBuenaVista.png)](https://travis-ci.org/brosmike/FortBuenaVista)
+
 FortBuenaVista
 ==============
 
